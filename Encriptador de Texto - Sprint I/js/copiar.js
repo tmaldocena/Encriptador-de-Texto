bbe6dc199c91808e018efda4ef7fd5e4d9cf0daf
Tomas Maldocena <104042642+tmaldocena@users.noreply.github.com>
2022-07-11 02:13:51 -0300

    Delete Encriptador de Texto - Sprint I directory

--- Encriptador de Texto - Sprint I/js/copiar.js
+++
@@ -1,8 +0,0 @@
-var textoSalida = document.querySelector("#result");
-
-var copyBoton = document.querySelector("#copyBoton");
-
-copyBoton.addEventListener("click", function(){
-    textoSalida.select();
-    document.execCommand("copy");
-});
